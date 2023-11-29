2146d3399f229869cbb8bf7e6d9759813d92559c
do-sg <do.seongyeong1@gmail.com>
2023-11-29 17:03:29 +0900

    feat : 기본 레이아웃 구성 진행

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BottomBar } from "../styles/footerstyle";
+
+const Footer = () => {
+  return <BottomBar>홍길동</BottomBar>;
+};
+
+export default Footer;
---
+++ src/styles/footerstyle.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const BottomBar = styled.footer`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  padding: 0px 16px 0px 4px;
+  background: #fef7ff;
+  align-items: center;
+  gap: 276px;
+  flex-shrink: 0;
+`;
