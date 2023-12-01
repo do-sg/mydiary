a3ced0462c90d141f84136c17f19a80cf9d5af8c
do-sg <do.seongyeong1@gmail.com>
2023-12-01 18:03:43 +0900

    feat: 기본 라우터 진행

--- src/pages/diary/DiaryEdit.js
+++ src/pages/diary/DiaryEdit.js
@@ -2,14 +2,20 @@ import React from "react";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import Footer from "../../components/Footer";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DiaryEdit = () => {
+  const navigate = useNavigate();
+  const handleClickMenu = () => {
+    navigate(-1);
+  };
   const param = useParams();
   console.log(param);
   return (
     <>
-      <Header>My Diary Edit</Header>
+      <Header handleClickMenu={handleClickMenu} icon="bt_back.svg">
+        My Diary Edit
+      </Header>
       <Main>
         <h2 style={{ textAlign: "center", color: "blue" }}>
           다이어리 수정화면{param.pk} 글 수정
